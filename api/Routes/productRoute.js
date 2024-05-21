7fcc967ba3f4a0319963e74b5978477ad95787b4
developerakz <Sw@t4322>
2024-05-21 03:40:13 -0700

    delete extra dep

--- api/Routes/productRoute.js
+++ api/Routes/productRoute.js
@@ -2,10 +2,8 @@ import express from 'express'
 import {createProduct } from '../Controller/productController.js'
 import { isAuthenticated } from '../middleware/auth.js'
 
-
 const router = express.Router()
 
 router.route('/createproduct').post(createProduct) //isAuthenticated
-
 
 export default router
